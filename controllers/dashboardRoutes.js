4f57f37332ebb2d53bf1f1364679c762d1f970ec
tierney03 <tierney_03@mac.com>
2022-11-30 19:24:22 -0600

    Filling out more routes

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -1,5 +1,5 @@
-
-
-
+const router = require("express").Router();
+const { User, Post } = require("../models");
+const withAuth = require("../utils/auth");
 
 module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,7 @@
-const router = require('express').Router();
-const homeRoutes = require('./homeRoutes');
+const router = require("express").Router();
+const homeRoutes = require("./homeRoutes");
+const dashboardRoutes = require("./dashboardRoutes");
 
-router.use(homeRoutes)
+router.use(homeRoutes);
 
 module.exports = router;
