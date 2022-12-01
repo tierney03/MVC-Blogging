efddf14872f899641fe327ba7f9cbe8d46a7ce4f
tierney03 <tierney_03@mac.com>
2022-11-30 20:03:15 -0600

    Updating syntax

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -2,23 +2,22 @@ const router = require("express").Router();
 const { Post } = require("../../models");
 const withAuth = require("../../utils/auth");
 
-
 router.post("/", withAuth, async (req, res) => {
-    try {
-        const newPost = await Post.create({
-            ...req.body,
-            user_id: req.session.user_id,
-        });
-        res.status(200).json(newPost);
-    } catch (err) {
-        res.status(400).json(err);
-    }
+  try {
+    const newPost = await Post.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+    res.status(200).json(newPost);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.get (pulling 1 post)
+// router.get (pulling 1 post)
 
-router.delete (delete a post)
+// router.delete (delete a post)
 
-router.put (update a post)
+// router.put (update a post)
 
 module.exports = router;
