930d8dc2d00eb55218145d7d954520e4ceb1ec60
tierney03 <tierney_03@mac.com>
2022-12-07 09:19:59 -0600

    Comment Post Routes, getting error

--- controllers/api/commentPostRoutes.js
+++ controllers/api/commentPostRoutes.js
@@ -1,9 +1,18 @@
+const router = require("express").Router();
+const { Comment } = require("../../models");
+const withAuth = require("../../utils/auth");
 
+router.post("/comment/:id", async (req, res) => {
+  try {
+    const postComment = await Comment.findByPk(req.params.id);
+    const comment = postComment.get({ plain: true });
 
-
-
-
-
-
-
+    res.render("commentPost", {
+      ...comment,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 module.exports = router;
